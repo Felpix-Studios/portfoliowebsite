e60a49faa2b6d90d4a7b0a717fe6def3598301d4
Kevin Li <jshiansongli@gmail.com>
2020-07-10 21:53:57 -0400

    init commit!

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+let ejs = require('ejs'); 
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+app.get('/projects',(req,res)=>{
+    res.render('projects');
+});
+app.get('/articles',(req,res)=>{
+    res.render('articles');
+});
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
+const port= process.env.PORT || 5000;
+
+
+app.listen(port,() => console.log(`Server started on port ${port}`));
