275a7f47ceb3f1fd6609120aaaa7d49b69a701c7
Kevin Li <jshiansongli@gmail.com>
2022-04-19 18:01:55 -0400

    verfcfel tests

--- index.js
+++ index.js
@@ -14,38 +14,35 @@ app.use('/static', express.static('public'))
 app.set('view engine', 'ejs');
 
 //Temp Object in place of DB+Schema
-// articles = [{
-//     title:"The Pilot Article",
-//     time:new Date('2020-08-06T23:12:00+00:00'),
-//     markdown:md.render(fs.readFileSync("articles/The_Pilot_Article.md").toString()),
-//     wordCount:fs.readFileSync("articles/The_Pilot_Article.md").toString().split(' ').length,
-//     slug:slugify("The_Pilot_Article",{lower:true,strict:true})
-// }];
-// articles.sort((a,b)=>b.time-a.time);
-
-// articles.forEach(element => {
-//     console.log(element.time);
-//     console.log(element.markdown)
-//     console.log(element.slug)
-// });
+articles = [{
+    title:"The Pilot Article",
+    time:new Date('2020-08-06T23:12:00+00:00'),
+    markdown:md.render(fs.readFileSync("public/articles/The_Pilot_Article.md").toString()),
+    wordCount:fs.readFileSync("public/articles/The_Pilot_Article.md").toString().split(' ').length,
+    slug:slugify("The_Pilot_Article",{lower:true,strict:true})
+}];
+articles.sort((a,b)=>b.time-a.time)
+articles.forEach(element => {
+    console.log(element.wordCount);
+});
 app.get('/',(req,res)=>{
     try{
         res.render('index');
     }catch(err){
-        res.render('oopse')
+        res.send('oopse')
     }
 });
 app.get('/projects',(req,res)=>{
     res.render('projects');
 });
-// app.get('/articles',(req,res)=>{
-//     res.render('articles',{articles:articles});
-// });
-// app.get('/articles/:id',(req,res)=>{
-//     const article = articles.find(element=>element.slug==req.params.id);
-//     if(article == null) res.redirect("/articles");
-//     res.render('article',{article:article})
-// });
+app.get('/articles',(req,res)=>{
+    res.render('articles',{articles:articles});
+});
+app.get('/articles/:id',(req,res)=>{
+    const article = articles.find(element=>element.slug==req.params.id);
+    if(article == null) res.redirect("/articles");
+    res.render('article',{article:article})
+});
 app.get('/about',(req,res)=>{
     res.render('about');
 });
